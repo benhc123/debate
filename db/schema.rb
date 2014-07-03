02e0569e5e5ef3c52fad2c6a71d9ec66c73f73ef
Will Holcomb <will@dhappy.org>
2014-07-02 20:37:02 -0400

    fixing for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140702181236) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "issues", force: true do |t|
     t.text     "title"
     t.text     "text"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20140702181236) do
     t.datetime "updated_at"
   end
 
-  add_index "issues", ["author_id"], name: "index_issues_on_author_id"
+  add_index "issues", ["author_id"], name: "index_issues_on_author_id", using: :btree
 
   create_table "theses", force: true do |t|
     t.integer  "issue_id"
@@ -33,8 +36,8 @@ ActiveRecord::Schema.define(version: 20140702181236) do
     t.integer  "position"
   end
 
-  add_index "theses", ["author_id"], name: "index_theses_on_author_id"
-  add_index "theses", ["issue_id"], name: "index_theses_on_issue_id"
+  add_index "theses", ["author_id"], name: "index_theses_on_author_id", using: :btree
+  add_index "theses", ["issue_id"], name: "index_theses_on_issue_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -53,8 +56,8 @@ ActiveRecord::Schema.define(version: 20140702181236) do
     t.integer  "role"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "votes", force: true do |t|
     t.integer  "votable_id"
@@ -68,7 +71,7 @@ ActiveRecord::Schema.define(version: 20140702181236) do
     t.datetime "updated_at"
   end
 
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
 end
