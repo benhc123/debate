8939da3e90221c61b209fd1dcf3778ee4e7a42ec
Will Holcomb <will@dhappy.org>
2014-07-08 00:35:19 -0400

    adding issue editing

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -1,9 +1,22 @@
-$( function() {
-    $('#theses ol').sortable()
-    // {
-    //    connectWith: ".connectedSortable"
-    //} )
-        .disableSelection()
+( function() {
+    function ready() {
+        $('#theses ol').sortable()
+        // {
+        //    connectWith: ".connectedSortable"
+        //} )
+            .disableSelection()
 
-    $('[data-toggle="tooltip"]').tooltip()
-} )
+        $('[data-toggle="tooltip"]').tooltip()
+        
+        $('#text .html').html( markdown.toHTML( $('#text textarea').val() ) )
+        
+        $('#text').click( function() {
+            $(this).find( '.html' ).hide()
+            $(this).find( '.edit' ).show()
+        } )
+    }
+    
+    $(ready)
+    $(document).on( 'page:load', ready )
+} )()
+
