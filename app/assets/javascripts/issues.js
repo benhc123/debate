5f2bb85d2b2be4fd8bac85004a20e70d2588e27a
Will Holcomb <will@dhappy.org>
2014-07-08 00:52:27 -0400

    prettying links

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -14,6 +14,12 @@
             $(this).find( '.html' ).hide()
             $(this).find( '.edit' ).show()
         } )
+
+        $('#text .cancel').click( function() {
+            var $text = $(this).parents( '#text' )
+            $text.find( '.html' ).show()
+            $text.find( '.edit' ).hide()
+        } )
     }
     
     $(ready)
