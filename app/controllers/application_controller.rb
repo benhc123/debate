76cc33ae00a2cf065c82d8dc7428308387b1f931
Josh Weinstein <weinstein.josh@gmail.com>
2014-08-01 13:50:10 +0700

    Fix so devise lets us save the user's name on sign-up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters_for_devise, if: :devise_controller?
 
   include Pundit
   protect_from_forgery
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  private
-  
+protected
+  def configure_permitted_parameters_for_devise
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+private
   def user_not_authorized
     if not current_user
       redirect_to new_user_session_path
