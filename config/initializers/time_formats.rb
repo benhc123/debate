3a3ef615194c4293ff655b56c2565afcc6dab6de
Josh Weinstein <weinstein.josh@gmail.com>
2014-08-01 12:29:02 +0700

    Zero-pad days in pretty time format for alignment.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,1 @@
-Time::DATE_FORMATS[:pretty] = lambda { |time| time.strftime("%_m/%e/%y") + " at " + time.strftime("%l:%M") + time.strftime("%p").downcase }
+Time::DATE_FORMATS[:pretty] = lambda { |time| time.strftime("%_m/%d/%y") + " at " + time.strftime("%l:%M") + time.strftime("%p").downcase }
