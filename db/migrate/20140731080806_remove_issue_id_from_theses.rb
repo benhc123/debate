32b0fb7459e9f66fadb329297dd2accd6c28f608
Josh Weinstein <weinstein.josh@gmail.com>
2014-08-04 17:33:09 +0700

    Amend migration such that theses on production are linked to their issues in the new schema.

--- db/migrate/20140731080806_remove_issue_id_from_theses.rb
+++ db/migrate/20140731080806_remove_issue_id_from_theses.rb
@@ -1,5 +1,15 @@
 class RemoveIssueIdFromTheses < ActiveRecord::Migration
-  def change
+  def up
+    Thesis.all.each do |thesis|
+      method = thesis.position == 0 ? :add_thesis_for : :add_thesis_against
+      issue_id = thesis.read_attribute(:issue_id)
+      issue = Issue.find(issue_id)
+      issue.send(method, thesis)
+    end
     remove_column :theses, :issue_id, :integer
+  end
+
+  def down
+    add_column :theses, :issue_id, :integer
   end
 end
