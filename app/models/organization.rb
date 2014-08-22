8456b27d2dc52aae4616d2e50ef3dac9574a4f7c
Ali Ismayilov <ali@ismailov.info>
2014-08-22 18:03:55 -0300

    Add Organization#display and validate presence of name

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,12 @@ class Organization < ActiveRecord::Base
   has_many :delegations, as: :voter
   has_many :delegation_entries, as: :delegate
 
+  validates :name, presence: true
+
   acts_as_tagger
   acts_as_voter
+
+  def display
+    name
+  end
 end
